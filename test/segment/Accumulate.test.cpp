7a6fefc7d83898110ea85395912390cc0412be44
Coki <srcn4frdm.c@gmail.com>
2022-03-24 15:08:52 +0900

    updated test for Accumulate

--- test/segment/Accumulate.test.cpp
+++ test/segment/Accumulate.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://atcoder.jp/contests/typical90/tasks/typical90_j"
+#define PROBLEM "https://atcoder.jp/contests/abc149/tasks/abc149_e"
 
 // #pragma GCC target("avx2")
 // #pragma GCC optimize("O3")
@@ -16,36 +16,37 @@ constexpr long double EPS = 1e-10;
 constexpr long double PI = M_PI;
 
 #include "../../src/macros.hpp"
-#include "../../src/common/listnd.hpp"
+#include "../../src/common/input.hpp"
 #include "../../src/common/print.hpp"
 
+#include "../../src/common/bisearch.hpp"
+#include "../../src/common/bisect.hpp"
 #include "../../src/segment/Accumulate.hpp"
 
 void solve() {
-    ll N;
-    cin >> N;
-    auto A = list2d(2, N, 0LL);
-    rep(i, N) {
-        ll c, p;
-        cin >> c >> p;
-        c--;
-        A[c][i] = p;
+    ll N, M;
+    cin >> N >> M;
+    auto A = LIST(N);
+
+    sort(ALL(A));
+    ll x = bisearch_min(0, INF, [&](ll m) {
+        ll cnt = 0;
+        rep(i, N) {
+            cnt += N - bisect_left(A, m-A[i]);
+        }
+        return cnt <= M;
+    });
+
+    Accumulate<ll> acc(A);
+    ll ans = 0, cnt = M;
+    rep(i, N-1, -1, -1) {
+        ll j = N - bisect_left(A, x-A[i]);
+        cnt -= j;
+        ans += acc.query(N-j, N) + A[i]*j;
     }
-
-    Accumulate<ll> acc1(A[0]), acc2(A[1]);
-
-    ll Q;
-    cin >> Q;
-    rep(_, Q) {
-        ll l, r;
-        cin >> l >> r;
-        l--;
-
-        vector<ll> res(2);
-        res[0] = acc1.query(l, r);
-        res[1] = acc2.query(l, r);
-        print(res);
-    }
+    // 端数
+    ans += (x-1) * cnt;
+    print(ans);
 }
 
 int main() {
