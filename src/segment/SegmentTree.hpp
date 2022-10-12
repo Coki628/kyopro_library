89deec34953e8c9a1a3af7bfe1406c7a4e7c679b
Coki <srcn4frdm.c@gmail.com>
2022-10-12 17:00:16 +0900

    lib update

--- src/segment/SegmentTree.hpp
+++ src/segment/SegmentTree.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "../base.hpp"
 
 template<typename Monoid, typename F>
---
+++ src/string/SuffixArray.hpp
@@ -0,0 +1,33 @@
+#include "../macros.hpp"
+#include "../segment/SegmentTree.hpp"
+#include "../acl/string.hpp"
+
+auto f = [](int a, int b) { return min(a, b); };
+
+struct SuffixArray {
+    int N;
+    string S;
+    bool use_seg;
+    vector<int> sa, rsa, lcp;
+    SegmentTree<int, decltype(f)> seg = get_segment_tree(f, MOD);
+
+    SuffixArray(string S, bool use_seg=true) : S(S), N(S.size()), use_seg(use_seg) {
+        sa = atcoder::suffix_array(S);
+        rsa.assign(N, 0);
+        rep(i, N) {
+            rsa[sa[i]] = i;
+        }
+        lcp = atcoder::lcp_array(S, sa);
+        if (use_seg) {
+            seg.build(lcp);
+        }
+    }
+
+    // S[i]とS[j]のLCPを取得
+    int get_lcp(int i, int j) {
+        assert(use_seg);
+        int l = rsa[i], r = rsa[j];
+        if (l > r) swap(l, r);
+        return seg.query(l, r);
+    }
+};
